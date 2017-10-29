fffb0f7a14e041eb248bfc778cf068cdb5cd7d39
nm-ann <noam.annenberg@gmail.com>
2017-10-28 23:47:06 -0400

    stuff

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
 	return render_template('register')
 
